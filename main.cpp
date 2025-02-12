484c81639c677a6576e4cbace4b096e412fc023d
saif <potelmyfreezy@gmail.com>
2025-02-12 16:11:41 +0200

    latest version

--- main.cpp
+++ main.cpp
@@ -16,8 +16,11 @@ void filterinvertimage();
 void filtermergeimage();
 void filterrotateimage();
 void filterflipimage();
+void filterDarkenandLightenImage();
+void filterdetectimage();
+void filterEnlargeImage();
 
-int main(){
+int main(){ 
     cout << "ahlan ya user ya habibi :)" << endl ;
     loadImage();
     cout << "pls enter file name of the image to process :" << endl;
@@ -26,35 +29,47 @@ int main(){
     cout << "3- merge filter" << endl ; 
     cout << "4- flip iamge" << endl ;
     cout << "5- rotate image " << endl ;
+    cout << "6- darken and lighten image " << endl ;
+    cout << "7- detect image edges " << endl ;
+    cout << "8- enlarge image " << endl ;
+    cout << "exit >> 0 " ;
     int choice ;
     cin >> choice ;
     switch (choice){
 
         case 1 :
             filterblackandwhite() ;
-            saveImage() ;
             break;
 
         case 2 :
             filterinvertimage() ;
-            saveImage() ;
             break;
 
         case 3 :
             filtermergeimage() ;
-            saveImage() ;
             break;
 
         case 4 : 
             filterflipimage() ;
-            saveImage() ;
             break;
 
         case 5 :
             filterrotateimage() ;
-            saveImage() ;
+            break;
+
+        case 6 :
+            filterDarkenandLightenImage() ;
+            break;
+
+        case 7 : 
+            filterdetectimage() ;
+            break;
+
+        case 8 : 
+            filterEnlargeImage() ;
             break;
     } 
+    saveImage() ;
 
 }
 
@@ -215,5 +230,102 @@ void filterflipimage(){
                     image[i][j] = image2[256-i][j] ;
                 }
             }
+    }
+}
+
+void filterDarkenandLightenImage(){
+    int choice ;
+    cout << "MAKE IT DARKER ---> 1" << endl ;
+    cout << "MAKE IT LIGHTER ----> 2" << endl ;
+    cin >> choice ;
+    switch (choice){
+        case 1 :
+            for(int i = 0 ; i < SIZE ; i++){
+                for(int j = 0 ; j < SIZE ; j++){
+                    image[i][j] = image[i][j] / 2 ;
+                }
+            }
+        case 2 :
+            for(int i = 0 ; i < SIZE ; i++){
+                for(int j = 0 ; j < SIZE ; j++){
+                    if(image[i][j] <= 163){
+                        image[i][j] = 255 ;
+                    }
+                    else{
+                        image[i][j] = image[i][j] + 90 ;
+                    }
+                }
+            }
+    }
+
+}
+
+void filterdetectimage(){
+    unsigned char detectimage[SIZE][SIZE] ;
+    for(int i = 0 ; i < SIZE ; i++){
+        for(int j = 0 ; j < SIZE ; j++){
+            detectimage[i][j] = 255 ;
+        }
+    }
+    for (int i = 1; i < 255 ; i++) {
+        for (int j = 1; j < 255 ; j++) {
+            if (image[i][j] == 0) {
+                if (image[i][j + 1] == 255 || image[i][j - 1] == 255 || image[i + 1][j] == 255 || image[i - 1][j] == 255) {
+                    detectimage[i][j] = 0 ;
+                }
+            }
+        }
+    }
+    for(int i = 0 ; i < SIZE ; i++){
+        for(int j = 0 ; j < SIZE ; j++){
+            image[i][j] = detectimage[i][j] ;
+        }
+    }
+}
+
+void filterEnlargeImage(){
+    cout << "Which quarter do you want to enlarge 1 , 2 , 3 , 4 : ";
+    int quarter;
+    cin >> quarter;
+
+    unsigned char enlargedImage[SIZE][SIZE];
+
+    int rows , col ;
+    switch (quarter) {
+        case 1:
+            rows = 0;
+            col = 0;
+            break;
+
+        case 2: 
+            rows = 0;
+            col = SIZE / 2;
+            break;
+
+        case 3: 
+            rows = SIZE / 2;
+            col = 0;
+            break;
+
+        case 4:
+            rows = SIZE / 2;
+            col = SIZE / 2;
+            break;
+
+        default:
+            cout << "Invalid quarter" << endl;
+            return;
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            enlargedImage[i][j] = image[rows + i / 2][col + j / 2];
+        }
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            image[i][j] = enlargedImage[i][j];
+        }
     }
 }
